8a08505c68b58926567c02a6ad94059aa451c089
Max Baranskyi <max.baranskyi@gmail.com>
2022-01-10 19:01:41 +0200

    ex9. simplify sorting

--- js/exercise_9/exercise_9.js
+++ js/exercise_9/exercise_9.js
@@ -1,9 +1,7 @@
 function ascDesNone(numberSets, order) {
     switch(order) {
         case 'Asc':
-            return numberSets.sort(function(a, b) {
-                return a - b;
-            });
+            return numberSets.sort();
         case 'Des':
             return numberSets.sort(function(a, b) {
                 return b - a;
