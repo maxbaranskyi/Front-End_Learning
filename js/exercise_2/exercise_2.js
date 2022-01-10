09c142d2fe0dba8f6872295e77fc87c07e691c71
Max Baranskyi <max.baranskyi@gmail.com>
2022-01-10 19:03:33 +0200

    ex2. use map function

--- js/exercise_2/exercise_2.js
+++ js/exercise_2/exercise_2.js
@@ -1,9 +1,3 @@
 function jazzify(notes) {
-    return notes.map(function(note) {
-        if (note.charAt(note.length - 1) !== '7') {
-            return note + '7';
-        } else {
-            return note;
-        }
-    });
+    return notes.map(note => note.substring(1) !== '7' ? `${note}7` : note);
 }
