b45ba289e02e5ec87391c0ff9035e814fb2a7a98
Max Baranskyi <max.baranskyi@gmail.com>
2022-01-10 19:00:42 +0200

    ex16. check if more that 0

--- js/exercise_15/exercise_15.js
+++ js/exercise_15/exercise_15.js
@@ -1,5 +1,5 @@
 function pentagonal(index) {
-    if (index >= 1) {
+    if (index > 0) {
         return (5 * index ** 2 - 5 * index + 2 ) / 2;
     } else {
         alert("Index number should be greater or equals 1");
